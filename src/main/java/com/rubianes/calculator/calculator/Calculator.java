8db4a6c847a5551a81be278d4242b00c4a90db63
Juan Pablo Rubianes <rubianes@adobe.com>
2018-01-25 15:51:34 -0500

    adding main program

---
+++ src/main/java/com/rubianes/calculator/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.rubianes.calculator.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a,int b){
+        return a + b;
+    }
+}
